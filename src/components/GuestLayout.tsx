c3a1a36d0826f30ce4b9e2423d820416b4620c98
Denys <denisligach98@gmail.com>
2023-11-06 23:52:44 +0200

    centered auth form

--- src/components/GuestLayout.tsx
+++ src/components/GuestLayout.tsx
@@ -11,7 +11,7 @@ const GuestLayout: FunctionComponent<Props> = () => {
   }
 
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <Outlet></Outlet>
     </div>
   )
--- src/views/Auth.tsx
+++ src/views/Auth.tsx
@@ -27,7 +27,6 @@ const Auth: FunctionComponent<Props> = ({ title, type }) => {
     e.preventDefault()
 
     try {
-      // dispatch(login({ email, password: pwd }))
       const data = await login({ email: email, password: pwd }).unwrap()
       dispatch(setCredentials(data))
       setEmail("")
@@ -44,7 +43,7 @@ const Auth: FunctionComponent<Props> = ({ title, type }) => {
     setPwd(e.target.value)
 
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div>
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           {title}
